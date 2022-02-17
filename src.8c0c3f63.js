587cac3f10e162c0a6337b2998df3820c58d515f
86andrey <92923368+86andrey@users.noreply.github.com>
2022-02-17 17:53:08 +0000

    Deploying to gh-pages from @ 86andrey/IceCream-project-12@3c8561fa8393601e60e6e69a1864d7652b0a026d 🚀

--- src.8c0c3f63.js
+++ src.8c0c3f63.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/IceCream-project-12/src.1c29bc16.js.map
+//# sourceMappingURL=/IceCream-project-12/src.8c0c3f63.js.map
