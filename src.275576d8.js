4b2f6b8c08c597344b706dbc30080a3a44eddd32
OlenkaSav <94956727+OlenkaSav@users.noreply.github.com>
2022-02-19 12:36:58 +0000

    Deploying to gh-pages from @ 86andrey/IceCream-project-12@9964a225e895fc97056f991708905ce557789d56 🚀

--- src.275576d8.js
+++ src.275576d8.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/IceCream-project-12/src.b84ea74e.js.map
+//# sourceMappingURL=/IceCream-project-12/src.275576d8.js.map
